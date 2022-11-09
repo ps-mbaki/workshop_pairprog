459b7b1f6b0a2b5787904d2e85ad015a2935e989
Mohamad Abdulbaki <mohamad.abdulbaki@publicissapient.com>
2022-11-09 16:25:24 +1100

    Pair programming Quang and Mohamad Added tests and implemented tennis game

--- TennisLibrary/Game.cs
+++ TennisLibrary/Game.cs
@@ -3,6 +3,7 @@ public class Game
 {
     int playerOneScore;
     int playerTwoScore;
+    bool over;
 
     List<int> scores;
 
@@ -11,11 +12,40 @@ public class Game
         playerTwoScore = 0;
 
         scores = new List<int>();
+
+        scores.Add(playerOneScore);
+        scores.Add(playerTwoScore);
+        over = false;
     }
 
     public List<int> getScore() {
         return scores;
     }
+
+    public bool isOver(){
+        return over;
+    }
+
+    public bool playerScores(int playerIndex) {
+        if (playerIndex > 1 || playerIndex < 0 || over)
+        {
+            return false;
+        }
+        scores[playerIndex] = scores[playerIndex] + 1;
+        checkWin();
+        return true;
+    }
     
+    public bool checkWin() {
+        if(scores[0] >= scores[1] + 2 && scores[0] >= 4) {
+            over = true;
+            return true;
+        }
+        else if(scores[1] >= scores[0] + 2 && scores[1] >= 4){
+            over = true;
+            return true;
+        }
+        return false;
+    }
 
 }
--- TennisLibraryTest/UnitTest1.cs
+++ TennisLibraryTest/UnitTest1.cs
@@ -6,17 +6,23 @@ namespace TennisLibraryTest;
 [TestClass]
 public class UnitTest1
 {
+
+    Game game;
+
+    [TestInitialize]
+    public void testInitialize() {
+        game = new Game();
+    }
+
     [TestMethod]
     public void testGameNotNull()
     {
-        Game game = new Game();
         Assert.IsNotNull(game);
         
     }
 
     [TestMethod]
     public void testScoreIsZero() {
-        Game game = new Game();
         List<int> scores = game.getScore();
 
         // Verify both scores start at 0
@@ -26,10 +32,52 @@ public class UnitTest1
 
     [TestMethod]
     public void testScoreIsNotNull() {
-        Game game = new Game();
         List<int> scores = game.getScore();
 
         // Verify both scores start at 0
         Assert.IsNotNull(scores);
     }
+
+    [TestMethod]
+    public void testPlayerScoresLargeIndexHandled() {
+        Assert.IsFalse(game.playerScores(5));
+    }
+
+    [TestMethod]
+    public void testPlayerOneScoresPoint() {
+        Assert.IsTrue(game.playerScores(0));
+        Assert.AreEqual(1, game.getScore()[0]);
+    }
+
+    [TestMethod]
+    public void testPlayerTwoScoresPoint() {
+        game.playerScores(1);
+        Assert.AreEqual(1, game.getScore()[1]);
+    }
+
+    [TestMethod]
+    public void checkGameIsOver() {
+        game.playerScores(1);
+        game.playerScores(1);
+        game.playerScores(1);
+        game.playerScores(0);
+        game.playerScores(1);
+
+        Assert.IsTrue(game.isOver());
+    }
+
+    [TestMethod]
+    public void checkGameIsOverDeuce() {
+        game.playerScores(1);
+        game.playerScores(1);
+        game.playerScores(1);
+        game.playerScores(0);
+        game.playerScores(0);
+        game.playerScores(0);
+        game.playerScores(1);
+        game.playerScores(1);
+
+        Assert.IsTrue(game.isOver());
+    }
+
 }
